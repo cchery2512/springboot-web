f510ebc1bd2f9b79d0e57a673b89a680852c7e5c
Carmelo Chery <cchery@solucionesinformaticasmj.com>
2025-10-16 17:00:47 +0200

    Api Reest enviar Json petición POST

--- src/main/java/com/cchery/curso/springboot/webapp/springboot_web/http/controllers/PathVariableController.java
+++ src/main/java/com/cchery/curso/springboot/webapp/springboot_web/http/controllers/PathVariableController.java
@@ -1,10 +1,16 @@
 package com.cchery.curso.springboot.webapp.springboot_web.http.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cchery.curso.springboot.webapp.springboot_web.models.User;
 import com.cchery.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 @RestController
@@ -15,5 +21,18 @@ public class PathVariableController {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathBar(@PathVariable String product, @PathVariable Long id){
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        return user;
     }
 }
